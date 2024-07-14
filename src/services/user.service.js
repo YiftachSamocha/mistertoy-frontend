3aba46f0e2fb705399faae9e6fc1ba7e6e636063
Yiftach Samocha <yiftachsamo@gmail.com>
2024-07-14 15:05:27 +0300

    connect to back user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,37 +1,33 @@
-import { storageService } from "./async-storage.service.js"
-import { utilService } from "./util.service.js"
-
-const DB_USERS = 'DB_USERS'
-const DB_CURR_USER = 'DB_CURR_USER'
+import { httpService } from "./http.service.js"
 export const userService = { login, signup, logout, getLoggedinUser }
+const BASE_URL = 'auth/'
+const DB_CURR_USER = 'DB_CURR_USER'
 
-function signup(user) {
-    user.createdAt = new Date()
-    user._id = utilService.makeId()
-    return storageService.post(DB_USERS, user)
-        .then(() => _setLoggedInUser(user))
-
+function login(user) {
+    return httpService.post(BASE_URL + 'login', user)
+        .then(userToSave => {
+            if (!userToSave) return Promise.reject('Cannot login')
+            return _setLoggedInUser(userToSave)
+        })
 }
 
-function login(userToLogin) {
-    return storageService.query(DB_USERS)
-        .then(users => {
-            const foundUser = users.find(user => user.password === userToLogin.password && user.username === userToLogin.username)
-            if (!foundUser) return Promise.reject('Cannot find user')
-            return _setLoggedInUser(foundUser)
+function signup(user) {
+    return httpService.post(BASE_URL + 'signup', user)
+        .then(userToSave => {
+            if (!userToSave) return Promise.reject('Cannot signup')
+            return _setLoggedInUser(userToSave)
         })
 }
 
 function logout() {
-    sessionStorage.clear()
-    return Promise.resolve()
+    return httpService.post(BASE_URL + 'logout')
+        .then(() => sessionStorage.clear())
 }
+
 
 function getLoggedinUser() {
     return JSON.parse(sessionStorage.getItem(DB_CURR_USER))
 }
-
-
 
 function _setLoggedInUser(user) {
     const userToSave = { fullname: user.fullname, _id: user._id }
