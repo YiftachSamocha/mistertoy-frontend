d39fb240c3a8a843589abcb892c0fc01f26a7df9
Yiftach Samocha <yiftachsamo@gmail.com>
2024-07-16 21:58:44 +0300

    UI start

--- src/cmps/ToyFilter.jsx
+++ src/cmps/ToyFilter.jsx
@@ -1,66 +1,106 @@
-import { useState } from "react"
 import { toyService } from "../services/toy.service.js"
 import { useSelector } from "react-redux"
 import { store } from "../store/store.js"
 import { SET_FILTER_BY } from "../store/reducers/toy.reducer.js"
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import { TextField } from "@mui/material";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+
 
 export function ToyFilter() {
     const filterBy = useSelector(state => state.toyModule.filterBy)
-    const [isLabelsOpened, setIsLabelsOpened] = useState(false)
     const labels = toyService.getLabels()
 
     function handleChange({ target }) {
-        let { name, value, selected, type, checked } = target
-        if (name === 'inStock' || name === 'sort') value === selected
-        if (type === 'checkbox') {
-            if (checked) store.dispatch({ type: SET_FILTER_BY, filterBy: { ...filterBy, labels: [...filterBy.labels, name] } })
-            else {
-                const updatedLabels = filterBy.labels.filter(label => label !== name)
-                store.dispatch({ type: SET_FILTER_BY, filterBy: { ...filterBy, labels: updatedLabels } })
-            }
-            return
-        }
+        let { name, value } = target
         store.dispatch({ type: SET_FILTER_BY, filterBy: { ...filterBy, [name]: value } })
     }
     return <section className="filterBy">
         <div>
-            <label htmlFor="name" >Name:</label>
-            <input type="text" id="name" name="name"
-                value={filterBy.name} onChange={handleChange} />
+            <TextField
+                id="name"
+                label="Name"
+                variant="outlined"
+                name="name"
+                value={filterBy.name}
+                onChange={handleChange}
+            />
         </div>
-        <div>
-            <label htmlFor="inStock">Stock</label>
-            <select name="inStock" id="inStock"
-                value={filterBy.inStock} onChange={handleChange} >
-                <option value="all">All</option>
-                <option value="in">In Stock</option>
-                <option value="out">Out of Stock</option>
-            </select>
-        </div>
-        <div>
-            <label htmlFor="sort">Sort by:</label>
-            <select name="sort" id="sort"
-                value={filterBy.sort} onChange={handleChange}>
-                <option value="name">Name</option>
-                <option value="price">Price</option>
-                <option value="date">Creation Time</option>
-            </select>
-        </div>
-        <div className="label-container">
-            <label htmlFor="lables">Labels:</label>
-            <button type="button" onClick={() => setIsLabelsOpened(!isLabelsOpened)}>
-                {isLabelsOpened ? 'Hide Labels' : 'Show Labels'}
-            </button>
-        </div>
-        {isLabelsOpened && <div id="labels"  >
-            {labels.map(label => {
-                return <div key={label}>
-                    <label htmlFor={label}>{label}</label>
-                    <input type="checkbox" name={label} onChange={handleChange}
-                        checked={filterBy.labels.includes(label)}></input>
-                </div>
-            })}
-        </div>}
 
+        <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">In Stock:</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={filterBy.inStock}
+                    label="Sort"
+                    onChange={handleChange}
+                    name="inStock"
+                >
+                    <MenuItem value="all">All</MenuItem>
+                    <MenuItem value="in">In Stock</MenuItem>
+                    <MenuItem value="out">Out of Stock</MenuItem>
+                </Select>
+            </FormControl>
+        </Box>
+        <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Sort</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={filterBy.sort}
+                    label="Sort"
+                    onChange={handleChange}
+                    name="sort"
+                >
+                    <MenuItem value="name">Name</MenuItem>
+                    <MenuItem value="price">Price</MenuItem>
+                    <MenuItem value="date">Creation time</MenuItem>
+                </Select>
+            </FormControl>
+        </Box>
+
+        <div>
+            <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel id="demo-multiple-name-label">Labels</InputLabel>
+                <Select
+                    labelId="demo-multiple-name-label"
+                    id="demo-multiple-name"
+                    multiple
+                    value={filterBy.labels}
+                    onChange={handleChange}
+                    input={<OutlinedInput label="Label" />}
+                    MenuProps={MenuProps}
+                    name="labels"
+                >
+                    {labels.map((label) => (
+                        <MenuItem
+                            key={label}
+                            value={label}
+                        >
+                            {label}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </div>
     </section>
 }
