8b809f83ff754f1ebdf0e5fdcc613c9850ec5980
Yiftach Samocha <yiftachsamo@gmail.com>
2024-08-08 19:41:42 +0300

    Styling

--- src/pages/ReviewExplore.jsx
+++ src/pages/ReviewExplore.jsx
@@ -1,23 +1,74 @@
 import { useEffect, useState } from "react";
-import { reviewService } from "../services/review";
+import { useSelector } from "react-redux";
+import { loadReviews, removeReview } from "../store/actions/review.actions.js";
+import { loadToys, saveToy } from "../store/actions/toy.actions";
+import { loadUsers } from "../store/actions/user.actions";
 
-export function ReviewExplore(){
-    const [reviews, setReviews]= useState([])
-    const filterBy= {}
-    useEffect(()=>{
-        reviewService.query(filterBy)
-        .then(foundReviews=> setReviews(foundReviews))
-    },[])
-    console.log(reviews)
+export function ReviewExplore() {
+    const reviews = useSelector(state => state.reviewModule.reviews)
+    const users = useSelector(state => state.userModule.users)
+    const toys = useSelector(state => state.toyModule.toys)
+    const currUser = useSelector(state => state.authModule.loggedInUser)
+    const [filterBy, setFilterBy] = useState({ txt: '', toy: '', user: '' })
+
+    useEffect(() => {
+        loadReviews(filterBy)
+            .then(() => loadToys())
+            .then(() => loadUsers())
+    }, [filterBy])
+    function handleChange({ target }) {
+        const { value, name } = target
+        setFilterBy({ ...filterBy, [name]: value })
+    }
+    function isDeleteable(review) {
+        if(!currUser) return false
+        if (currUser.isAdmin) return true
+        if (currUser._id === review.byUser._id) return true
+        return false
+    }
+
+    async function onRemoveReview(reviewId) {
+        //Remove from review
+        await removeReview(reviewId)
+
+        
+        
+    }
 
     return <section className="reviews-explore">
         <h1>Reviews</h1>
-        <div>
-            {reviews.map(review=>{
+        <div className="reviews-filter">
+            <div>
+                <label htmlFor="txt">Text</label>
+                <input type="text" name="txt" id="txt"
+                    value={filterBy.txt} onChange={handleChange} />
+            </div>
+            <div>
+                <label htmlFor="toy">Toy</label>
+                <select name="toy" id="toy" value={filterBy.toy} onChange={handleChange}>
+                    <option value=''>All</option>
+                    {toys.map(toy => {
+                        return <option value={toy._id} key={toy._id}>{toy.name}</option>
+                    })}
+                </select>
+            </div>
+            <div>
+                <label htmlFor="user">User</label>
+                <select name="user" id="user" value={filterBy.user} onChange={handleChange}>
+                    <option value=''>All</option>
+                    {users.map(user => {
+                        return <option value={user._id} key={user._id}>{user.fullname}</option>
+                    })}
+                </select>
+            </div>
+        </div>
+        <div className="reviews-content">
+            {reviews.map(review => {
                 return <div className="review-preview" key={review._id}>
                     <h2>By User: {review.byUser.fullname}</h2>
                     <h2>About Toy: {review.aboutToy.name}</h2>
                     <p>{review.txt}</p>
+                    {isDeleteable(review) && <button onClick={() => onRemoveReview(review._id)}><i className="fa-solid fa-trash"></i></button>}
                 </div>
             })}
         </div>
--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,28 +1,26 @@
-import { removeReview } from "../store/actions/review.actions.js"
-import { saveToy } from "../store/actions/toy.actions"
+import { loadReviews, removeReview } from "../store/actions/review.actions.js"
 import { userService } from "../services/user/index.js"
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { authService } from "../services/auth/index.js"
-import { reviewService } from "../services/review/index.js"
+import { useSelector } from "react-redux"
+
 
 export function UserDetails() {
     const [user, setUser] = useState(authService.getLoggedinUser())
-    const [reviews, setReviews] = useState([])
+    const reviews = useSelector(state => state.reviewModule.reviews)
     const userId = useParams().id
     useEffect(() => {
         userService.getById(userId)
             .then(foundUser => setUser(foundUser))
-            .then(() => reviewService.query({ user: user._id }))
-            .then(userReviews => setReviews(userReviews))
+            .then(() => loadReviews(user._id))
     }, [])
 
 
-    // async function onRemoveReview(review) {
-    //     await removeReview(review._id)
+    async function onRemoveReview(reviewId) {
+        await removeReview(reviewId)
 
-    //     await saveToy()
-    // }
+    }
     if (!user) return <div>Log in to watch your reviews!</div>
     return <section className="user-details">
         <h1>{user.fullname}: Reviews</h1>
@@ -31,7 +29,7 @@ export function UserDetails() {
                 return <div key={review._id} className="user-review">
                     <h2>{review.aboutToy.name}</h2>
                     <p>{review.txt}</p>
-                    {/* <button onClick={() => onRemoveReview(review._id)}><i className="fa-solid fa-trash"></i></button> */}
+                    <button onClick={() => onRemoveReview(review._id)}><i className="fa-solid fa-trash"></i></button>
                 </div>
             })}
         </div>
