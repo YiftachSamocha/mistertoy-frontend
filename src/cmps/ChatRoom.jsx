e1051bb6dec2fcbe35bde43068724082c1122a9d
Yiftach Samocha <yiftachsamo@gmail.com>
2024-08-14 22:22:08 +0300

    Typing

--- src/cmps/ChatRoom.jsx
+++ src/cmps/ChatRoom.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { SOCKET_EMIT_SEND_MSG, SOCKET_EMIT_SET_TOPIC, SOCKET_EVENT_ADD_MSG, socketService } from "../services/socket.service"
+import { SOCKET_EMIT_SEND_MSG, SOCKET_EMIT_SET_TOPIC, SOCKET_EVENT_ADD_MSG, SOCKET_EVENT_USET_TYPING, socketService } from "../services/socket.service"
 import { useSelector } from "react-redux"
 import { utilService } from "../services/util.service.js"
 
@@ -7,6 +7,7 @@ export function ChatRoom({ toy }) {
     const [currTxt, setCurrTxt] = useState('')
     const currUser = useSelector(state => state.authModule.loggedInUser)
     const [msgs, setMsgs] = useState([])
+    const [typingUsers, setTypingUsers] = useState([])
 
     useEffect(() => {
         socketService.on(SOCKET_EVENT_ADD_MSG, addMsg)
@@ -18,12 +19,42 @@ export function ChatRoom({ toy }) {
         socketService.emit(SOCKET_EMIT_SET_TOPIC, toy._id)
     }, [toy])
 
+    useEffect(() => {
+        socketService.on(SOCKET_EVENT_USET_TYPING, setTypingUser)
+
+        return () => {
+            socketService.off(SOCKET_EVENT_USET_TYPING, setTypingUser)
+        }
+    }, [])
+
     function addMsg(msg) {
-        setMsgs(lastMsgs => [...lastMsgs, msg]) 
+        setMsgs(lastMsgs => [...lastMsgs, msg])
+    }
+
+    function setTypingUser(user) {
+        if (user.stopped) {
+            removeTypingUser(user)
+            return
+        }
+        setTypingUsers(prev => {
+            if (prev.some(u => u._id === user._id)) return prev
+            return [...prev, user]
+        })
+    }
+    function removeTypingUser(user) {
+        setTypingUsers(prev => prev.filter(u => u._id !== user._id))
     }
     function handleChange({ target }) {
         const { value } = target
         setCurrTxt(value)
+        socketService.emit(SOCKET_EVENT_USET_TYPING, currUser)
+
+        clearTimeout(window.typingTimeout)
+
+        window.typingTimeout = setTimeout(() => {
+            removeTypingUser(currUser)
+            socketService.emit(SOCKET_EVENT_USET_TYPING, { ...currUser, stopped: true })
+        }, 2000)
     }
     function sendMsg() {
         socketService.emit(SOCKET_EMIT_SEND_MSG, createMsg())
@@ -38,15 +69,22 @@ export function ChatRoom({ toy }) {
         }
     }
 
-    function filterMsgs(){
+    function filterMsgs() {
         return msgs.filter(msg => msg.toyId === toy._id)
     }
 
     if (!currUser) return <div>Log in to enter the chat!</div>
     return <section>
         <div>
+            {typingUsers.map(user => <span>{user.fullname} </span>)}
+            {typingUsers.length === 0 ? '' : ' is typing...'}
+        </div>
+        <div>
             {filterMsgs().map(msg => {
-                return <div key={utilService.makeId()}><b>{msg.by.fullname}</b>{msg.txt}</div>
+                return <div key={utilService.makeId()}>
+                    <b>{msg.by.fullname === currUser.fullname ? 'Me' : msg.by.fullname}</b>
+                    {msg.txt}
+                </div>
             })}
         </div>
 
